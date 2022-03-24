0f17aec8c721ddd4846e78cdfb857086a8f0e66f
MaxSykharev <maxsykharev@gmail.com>
2022-03-24 23:48:36 +0300

    added basename for routers

--- crmsystem/urls.py
+++ crmsystem/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
 urls to all ViewSet 
 """
 router = routers.SimpleRouter()
-router.register('api/EmployeeList', EmployeeViewSet)
-router.register('api/CompaniesList', CompaniesViewSet)
-router.register('api/PositionsList', PositionsViewSet)
+router.register('api/EmployeeList', EmployeeViewSet, basename='employees')
+router.register('api/CompaniesList', CompaniesViewSet, basename='companies')
+router.register('api/PositionsList', PositionsViewSet, basename='positions')
 
 urlpatterns += router.urls
