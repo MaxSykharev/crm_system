0c47421d9e64beac406fc48f3e0ed5772dd0391d
MaxSykharev <maxsykharev@gmail.com>
2022-03-24 09:57:29 +0300

    added mixins

--- crmforapp/views.py
+++ crmforapp/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
@@ -7,19 +7,27 @@ from .models import Employee, Company
 from .serializer import EmployeeSerializer, CompanySerializer
 
 
-class EmployeeAPIView(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    queryset = Employee.objects.all()
+class EmployeeViewSet(mixins.CreateModelMixin,
+                      mixins.ListModelMixin,
+                      mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      viewsets.GenericViewSet):
     serializer_class = EmployeeSerializer
+    queryset = Employee.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filter_fields = ['second_name', 'company']
     search_fields = ['second_name', 'first_name']
 
 
-class CompaniesAPIView(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    queryset = Company.objects.all()
+class CompaniesViewSet(mixins.CreateModelMixin,
+                       mixins.ListModelMixin,
+                       mixins.RetrieveModelMixin,
+                       mixins.UpdateModelMixin,
+                       viewsets.GenericViewSet):
     serializer_class = CompanySerializer
+    queryset = Company.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filter_fields = ['friends']
     search_fields = ['name', 'id']
