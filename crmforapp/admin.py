b51c2f83d6c4173e54bb53405089cc1a62176e73
MaxSykharev <maxsykharev@gmail.com>
2022-03-25 09:39:12 +0300

    fixed spaces

--- crmforapp/admin.py
+++ crmforapp/admin.py
@@ -11,15 +11,13 @@ register  models
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
     filter_horizontal = ('partners',)
-    list_display = ['name', 'country', 'email', 'count_employees', 'get_employees', 'created_at',
-                    'last_changes']
+    list_display = ['name', 'country', 'email', 'count_employees', 'get_employees', 'created_at','last_changes']
 
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
     filter_horizontal = ('company', 'profession',)
-    list_display = ['first_name', 'second_name', 'age', 'knowledge', 'phone_number',
-                    'email', 'start_work', ]
+    list_display = ['first_name', 'second_name', 'age', 'knowledge', 'phone_number','email', 'start_work', ]
 
 
 @admin.register(Position)
